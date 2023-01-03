232594b40f13c4b098428c570a9331318c0e6abd
AndreaHiott <66185876+AndreaHiott@users.noreply.github.com>
2023-01-03 20:14:30 +0100

    added java from another file, need to repair

--- src/scripts.js
+++ src/scripts.js
@@ -1,49 +1,60 @@
-let weather = {
-  "paris": {
-    temp: 19.7,
-    humidity: 80
-  },
-  "tokyo": {
-    temp: 17.3,
-    humidity: 50
-  },
-  "lisbon": {
-    temp: 30.2,
-    humidity: 20
-  },
-  "sanfrancisco": {
-    temp: 20.9,
-    humidity: 100
-  },
-  "moscow": {
-    temp: -5,
-    humidity: 20
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+let h3 = document.querySelector("h3");
+h3.innerHTML = `${day}, ${hours}:${minutes}`;
+
+function showTemperature(response) {
+  let city = document.querySelector("#enter-city").value;
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = `Currently ${temperature} °C `;
+}
+function searchCity(city) {
+  let apiKey = "ea600dd4b1532851054fb72f18bd3426";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+function searchPlace(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector(".enter-city");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${searchInput.value}`;
+  searchCity(searchInput.value);
+}
+let form = document.querySelector("#change-city");
+form.addEventListener("submit", searchPlace);
+
+function showResponse(response) {
+  let h1 = document.querySelector("h1");
+  let temperature = Math.round(response.data.main.temp);
+  h1.innerHTML = `${temperature} °C ${response.data.name}`;
+}
+function showPosition(event) {
+  if (event.preventDefault) {
+    event.preventDefault();
   }
-};
-
-
-let city = prompt("Where are you, or where do you want to go?");
-         city=city.toLowerCase();
-         city=city.replace("sanfrancisco", "san francisco");
-         console.log(Math.round(weather.paris.temp));
-
-
-function cityStuff() {
-        if (city==="paris") {
-           alert(`It is now ${Math.round(weather.paris.temp)} degrees with a humidity of ${weather.paris.humidity} in Paris! `);
-         } else
-         if (city==="lisbon") {
-          alert(`It is now ${Math.round(weather.lisbon.temp)} degrees with a humidity of ${weather.lisbon.humidity} in Lisbon!`);
-         } else
-         if (city==="tokyo") {
-        alert(`It is now ${Math.round(weather.tokyo.temp)} degrees with a humidity of ${Math.round(weather.tokyo.temp)} in Tokyo!`);
-         } else
-         if (city==="san francisco") {
-        alert(`It is now ${Math.round(weather.sanfrancisco.temp)} degrees with a humidity of ${weather.sanfrancisco.humidity} in San Francisco!`);
-         } else
-         if (city==="moscow") {
-        alert(`It is now ${Math.round(weather.moscow.temp)} degrees with a humidity of ${weather.moscow.humidity} in Moscow!`);
-         } else {
-        alert("I don't know that city friend. Try searching www.google.com");}
-      }
-      cityStuff();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+function searchLocation(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "ea600dd4b1532851054fb72f18bd3426";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showResponse);
+}
+navigator.geolocation.getCurrentPosition(showPosition);
+let button = document.querySelector("#current-loc");
+button.addEventListener("click", showPosition);
